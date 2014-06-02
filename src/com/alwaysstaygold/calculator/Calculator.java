535f9a0b9f68eb384085a218b121496addb2dd22
alwaysstaygold <alwaysstaygold2@gmail.com>
2014-06-01 20:51:21 -0700

    Worked on simplification

--- src/com/alwaysstaygold/calculator/Calculator.java
+++ src/com/alwaysstaygold/calculator/Calculator.java
@@ -41,37 +41,23 @@ public class Calculator {
 		System.out.println("4. Divide");
 		SimpleMath SimpleMath = new SimpleMath();
 		String choice = sc.nextLine();
+		System.out.println("Type in a number");
+		double num1 = sc.nextDouble();
+		System.out.println("Now another number please");
+		double num2 = sc.nextDouble();
 		if (choice.equalsIgnoreCase("add") || Integer.parseInt(choice) == 1){
-			System.out.println("Give me a number.");
-			double num1 = sc.nextDouble();
-			System.out.println("Mmmmmmm numbers! Give me another number!");
-			double num2 = sc.nextDouble();
 			double sum = SimpleMath.addition(num1, num2);
 			System.out.println("Your final number is " + sum);
 		}
 		else if (choice.equalsIgnoreCase("Subtract") || Integer.parseInt(choice) == 2){
-			System.out.println("Sub... sub... subtraction scares me! But I'll do it anyway! Just for you!");
-			System.out.println("Give me a number please");
-			double num1 = sc.nextDouble();
-			System.out.println("And another one");
-			double num2 = sc.nextDouble();
 			double sum = SimpleMath.subtraction(num1, num2);
 			System.out.println("the answer is " + sum);
 		}
 		else if (choice.equalsIgnoreCase("Multiply") || Integer.parseInt(choice) == 3){
-			System.out.println("I would like to request a number of you");
-			double num1 = sc.nextDouble();
-			System.out.println("Thank you. Give me another and I'll multiply them together!");
-			double num2 = sc.nextDouble();
 			double product = SimpleMath.multiplication(num1, num2);
-			System.out.println("Your final number is " + product);
-			
+			System.out.println("Your final number is " + product);	
 		}
 		else if (choice.equalsIgnoreCase("Divide") || Integer.parseInt(choice) == 4){
-			System.out.println("Type in a number");
-			double num1 = sc.nextDouble();
-			System.out.println("One more number then I'll do the dividing thing!");
-			double num2 = sc.nextDouble();
 			double quotient = SimpleMath.division(num1, num2);
 			System.out.println("The quotient is " + quotient);
 		}
@@ -79,6 +65,7 @@ public class Calculator {
 			System.out.println("Oh dear oh dear!! I believe something went horribly wrong!");
 		}
 	}
+	
 	
 	private void formulasMenu() {
 		System.out.println("Would you like to use/find:");
